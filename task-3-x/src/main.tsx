c04f04ac6d4f87c798d9d36f0874269fb4d3ec49
Oleksandr Sereda <sereda.san@gmail.com>
2023-07-13 10:57:50 +0300

    Add gh-pages deployment

--- task-3-x/src/main.tsx
+++ task-3-x/src/main.tsx
@@ -1,65 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import Modal from 'react-modal';
-import { store } from 'store';
 import { App } from 'components/App';
 import { Posts } from 'features/posts';
 import { Todos } from 'features/todos';
-import { Users } from 'features/users';
 import {
-  UserDetails,
   AboutUser,
   UserAlbums,
+  UserDetails,
   UserPosts,
   UserTodos,
 } from 'features/user-details';
+import { Users } from 'features/users';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+import Modal from 'react-modal';
+import { Provider } from 'react-redux';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { store } from 'store';
 import './main.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Posts />,
-      },
-      {
-        path: 'todos',
-        element: <Todos />,
-      },
-      {
-        path: 'users',
-        element: <Users />,
-      },
-      {
-        path: 'users/:id',
-        element: <UserDetails />,
-        children: [
-          {
-            index: true,
-            element: <AboutUser />,
-          },
-          {
-            path: 'albums',
-            element: <UserAlbums />,
-          },
-          {
-            path: 'todos',
-            element: <UserTodos />,
-          },
-          {
-            path: 'posts',
-            element: <UserPosts />,
-          },
-        ],
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Posts />,
+        },
+        {
+          path: 'todos',
+          element: <Todos />,
+        },
+        {
+          path: 'users',
+          element: <Users />,
+        },
+        {
+          path: 'users/:id',
+          element: <UserDetails />,
+          children: [
+            {
+              index: true,
+              element: <AboutUser />,
+            },
+            {
+              path: 'albums',
+              element: <UserAlbums />,
+            },
+            {
+              path: 'todos',
+              element: <UserTodos />,
+            },
+            {
+              path: 'posts',
+              element: <UserPosts />,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  { basename: '/fox-react/' }
+);
 
 Modal.setAppElement('#root');
 
